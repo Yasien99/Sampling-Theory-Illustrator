d6d1681ff6aecbc58d559a38b297a8345ef09b68
Yasien Essam <yasien.essam99@gmail.com>
2021-11-06 14:46:17 +0200

    final

--- main.py
+++ main.py
@@ -1,76 +1,118 @@
+from PyQt5 import QtGui, QtWidgets, uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget,QPushButton, QVBoxLayout, QFileDialog, QGraphicsView
 import sys
-import numpy as np
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5 import uic
-import pyqtgraph as pg
-from PyQt5.QtWidgets import QFileDialog
-from numpy import linspace
-from pyqtgraph import PlotWidget, plot
-from math import *
+from PyQt5.uic.properties import QtCore
 from numpy import linspace, cos, sin, pi, ceil, floor, arange
+import numpy
+import pandas as pd
+from scipy import signal 
+SignalsCounter = -1
+signals_components = []
 
-
-class MainWindow(QtWidgets.QMainWindow):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        uic.loadUi("sample.ui", self)
+class Ui(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Ui, self).__init__()
+        uic.loadUi('sample.ui', self)
+        self.setWindowIcon(QtGui.QIcon("appicon.png"))
+        self.setWindowTitle("Sample illust")
         self.Upload.clicked.connect(self.open)
         self.Plotdata.clicked.connect(self.plot)
-        self.graph1.setBackground('black')
-        self.graph2.setBackground('black')
         self.SamplingPoints.clicked.connect(self.plotSamples)
         self.ShowSampledGraph.clicked.connect(self.plotSeparateSamples)
+        self.PlotComposer.clicked.connect(self.composer)
+        self.confirm.clicked.connect(self.composerSum)
+        self.horizontalSlider.setMaximum(600)
+        self.horizontalSlider.valueChanged.connect(self.plotSamples)
         self.clearSample.clicked.connect(self.clear)
+        self.showSample.clicked.connect(self.showw)
+        self.Remove.clicked.connect(self.remove)
         self.show()
-        self.horizontalSlider.setMaximum(80)
 
     def open(self):
+        global data
         path = QFileDialog.getOpenFileName(self, 'Open a file', '', 'All Files (*.*)')
         if path != ('', ''):
-            print("File path : " + path[0])
-
+            data = path[0]
+            print("File path: " + data)
+            
+##########################################################
+        data1 = pd.read_csv(data)
+        t = data1['# t']
+        x = data1['x']
+        spec = numpy.fft.rfft(x)
+        peak = numpy.argmax(spec)
+        val = numpy.abs(peak) # Find magnitude
+        print(val/6)
+        self.horizontalSlider.setMaximum((val/6)*3*6)
+ ########################################################## 
     def plot(self):
-        # sampling a signal badlimited to 40 Hz
-        # with a sampling rate of 800 Hz
-        f = 40;  # Hz
-        tmin = -1
-        tmax = 1
-        t = linspace(tmin, tmax, 900)
-        x = cos(2 * pi * t) + cos(2 * pi * f * t)  # signal sampling
-        self.graph1.plot(t, x, pen='black')
+        global data
+        global data1
+        data1 = pd.read_csv(data)
+       
+   
+        self.graph1.clear()
+        self.graph1.plot(data1['# t'], data1['x'] , pen='b')
+        self.graph1.setBackground('black')
 
     def plotSamples(self):
         self.graph1.clear()
-        # sampling the signal with a sampling rate of 80 Hz
-        # in this case, we are using the Nyquist rate.
-        f = 40;  # Hz
-        tmin = -1
-        tmax = 1
-        T = 1 / self.horizontalSlider.value()
-        nmin = ceil(tmin / T)
-        nmax = floor(tmax / T)
-        n = arange(nmin, nmax)
-        x1 = cos(2 * pi * n * T) + cos(2 * pi * f * n * T)
-        self.graph1.plot(n * T, x1, symbol='o', pen='b')
+        data1 = pd.read_csv(data)
+        x = data1['# t']
+        y = data1['x']
+        #print(len(y))
+        slider=self.horizontalSlider.value()
+        global f
+        global xnew
+        f = signal.resample(y, slider)
+        #print(f)
+        xnew = numpy.linspace(-3, 3, slider)
+        self.graph1.plot(data1['# t'], data1['x'], pen='b')
+        self.graph1.plot(xnew, f, symbol='o', pen='r')
 
     def plotSeparateSamples(self):
         self.graph2.clear()
-        f = 40;  # Hz
-        tmin = -1;
-        tmax = 1;
-        T = 1 / self.horizontalSlider.value();
-        nmin = ceil(tmin / T);
-        nmax = floor(tmax / T);
-        n = arange(nmin, nmax);
-        x1 = cos(2 * pi * n * T) + cos(2 * pi * f * n * T);
-        self.graph2.plot(n * T, x1, pen='b')
+        self.graph2.plot(xnew, f, pen='r')
+        self.graph2.setBackground('black')
+
+    def composer(self):
+        global SignalsCounter
+        self.graph3.clear()
+        tmin = -3;
+        tmax = 3;
+        global time
+        time = linspace(tmin, tmax, 900);
+        freq = int(self.FreqTextBox.text())
+  ########################################      
+       # SignalsFreq.append(freq)
+      #  print(SignalsFreq)
+       # self.horizontalSlider.setMaximum(3*6*5*max(SignalsFreq))
+ ##########################################################       
+        amp = int(self.AmpTextBox.text())
+        phase = int(self.PhaseTextBox.text())
+        global signals_components
+        signal_part = amp * sin(2 * pi * freq * time + phase)
+        signals_components.append(signal_part)
+        self.ComboBox.addItem(str(SignalsCounter + 1))
+        self.graph3.plot(time, signal_part, pen='orange')
+        SignalsCounter = SignalsCounter + 1
+
+    def composerSum(self):
+        self.graph4.clear()
+        self.graph4.plot(time, sum(signals_components), pen='green')
+        SavedSignal = numpy.asarray([ time,sum(signals_components)])
+        numpy.savetxt('Generated Signal'+str(SignalsCounter)+'.csv', SavedSignal.T,header="t,x", delimiter=",")
+        
+    def remove(self):
+        global SignalsCounter
+        self.ComboBox.removeItem(self.ComboBox.currentIndex())
+        signals_components.pop(self.ComboBox.currentIndex())        
+    def showw(self):
+        self.splitter.setGeometry(0, 0, 850, 780)
     def clear(self):
-        self.splitter.setGeometry(QtCore.QRect(60, 10, 610, 1300))
-        self.splitter.setOrientation(QtCore.Qt.Vertical)
+        self.splitter.setGeometry(0,0,850,1560)
 
-
+app = 0
 app = QtWidgets.QApplication(sys.argv)
-window = MainWindow()
-window.show()
+window = Ui()
 app.exec_()
